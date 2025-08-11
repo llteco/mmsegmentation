7d819d5246ef88cf22ec796a01c129b3b45f9bad
Wenyi Tang <wenyi.tang@intel.com>
2025-08-11 16:17:28 +0800

    [dev] make SegDataPreProcessor support 4D video
    Normally `SegDataPreProcessor` only can take 3D tensors
    as image data. Now it supports to take 4D tensors as
    video data, the 4D tensor should have format (T, C, H, W)

--- mmseg/utils/misc.py
+++ mmseg/utils/misc.py
@@ -1,5 +1,5 @@
 # Copyright (c) OpenMMLab. All rights reserved.
-from typing import List, Optional, Union
+from typing import List, Optional, Tuple, Union
 
 import numpy as np
 import torch
@@ -32,13 +32,14 @@ def stack_batch(inputs: List[torch.Tensor],
                 size: Optional[tuple] = None,
                 size_divisor: Optional[int] = None,
                 pad_val: Union[int, float] = 0,
-                seg_pad_val: Union[int, float] = 255) -> torch.Tensor:
+                seg_pad_val: Union[int, float] = 255
+                ) -> Tuple[torch.Tensor, SampleList]:
     """Stack multiple inputs to form a batch and pad the images and gt_sem_segs
     to the max shape use the right bottom padding mode.
 
     Args:
         inputs (List[Tensor]): The input multiple tensors. each is a
-            CHW 3D-tensor.
+            CHW 3D-tensor or TCHW 4D-tensor.
         data_samples (list[:obj:`SegDataSample`]): The list of data samples.
             It usually includes information such as `gt_sem_seg`.
         size (tuple, optional): Fixed padding size.
@@ -47,7 +48,7 @@ def stack_batch(inputs: List[torch.Tensor],
         seg_pad_val (int, float): The padding value. Defaults to 255
 
     Returns:
-       Tensor: The 4D-tensor.
+       Tensor: The 4D-tensor (images) or 5D-tensor (videos).
        List[:obj:`SegDataSample`]: After the padding of the gt_seg_map.
     """
     assert isinstance(inputs, list), \
@@ -55,11 +56,17 @@ def stack_batch(inputs: List[torch.Tensor],
     assert len({tensor.ndim for tensor in inputs}) == 1, \
         f'Expected the dimensions of all inputs must be the same, ' \
         f'but got {[tensor.ndim for tensor in inputs]}'
-    assert inputs[0].ndim == 3, f'Expected tensor dimension to be 3, ' \
+    assert inputs[0].ndim in (3, 4), \
+        f'Expected tensor dimension to be 3 or 4, ' \
         f'but got {inputs[0].ndim}'
     assert len({tensor.shape[0] for tensor in inputs}) == 1, \
         f'Expected the channels of all inputs must be the same, ' \
         f'but got {[tensor.shape[0] for tensor in inputs]}'
+    is_video = inputs[0].ndim == 4
+    if is_video:
+        assert len({tensor.shape[1] for tensor in inputs}) == 1, \
+            f'Expected the channels of all inputs must be the same, ' \
+            f'but got {[tensor.shape[1] for tensor in inputs]}'
 
     # only one of size and size_divisor should be valid
     assert (size is not None) ^ (size_divisor is not None), \
@@ -74,8 +81,7 @@ def stack_batch(inputs: List[torch.Tensor],
         max_size = (max_size +
                     (size_divisor - 1)) // size_divisor * size_divisor
 
-    for i in range(len(inputs)):
-        tensor = inputs[i]
+    for i, tensor in enumerate(inputs):
         if size is not None:
             width = max(size[-1] - tensor.shape[-1], 0)
             height = max(size[-2] - tensor.shape[-2], 0)
